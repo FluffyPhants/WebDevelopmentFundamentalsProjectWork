7947fc671b04ddf8ba186b026c4e9189cab185e1
Sebastian <zese1798@student.ju.se>
2021-09-16 16:40:30 +0200

    started working on implementing the dynamic website

--- ProjectWebsite/app.js
+++ ProjectWebsite/app.js
@@ -1,6 +1,31 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
+
+const projects =  [
+    {
+        title: "project 1",
+        subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
+        img: "https://bulma.io/images/placeholders/1280x960.png"
+    },
+    {
+        title: "project 2",
+        subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
+        img: "https://bulma.io/images/placeholders/1280x960.png"
+    },
+    {
+        title: "project 3",
+        subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
+        img: "https://bulma.io/images/placeholders/1280x960.png"
+    },
+    {
+        title: "project 4",
+        subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
+        img: "https://bulma.io/images/placeholders/1280x960.png"
+    }
+
+]
+
 const app = express()
 
 app.use(express.static('static'))
@@ -14,7 +39,10 @@ app.get('/', function(request, response){
 })
 
 app.get('/projects', function(request, response){
-    response.render('projects.hbs')
+    const model = {
+        projects
+    }
+    response.render('projects.hbs', model)
 })
 
 app.get('/about', function(request, response){
