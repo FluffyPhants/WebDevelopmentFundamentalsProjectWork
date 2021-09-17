8b5e65aa780518757d6c3df4f29917d9f8eb9720
Sebastian <zese1798@student.ju.se>
2021-09-17 18:40:15 +0200

    added individual project pages for each project

--- ProjectWebsite/app.js
+++ ProjectWebsite/app.js
@@ -4,21 +4,25 @@ const expressHandlebars = require('express-handlebars')
 
 const projects =  [
     {
+        id: 1,
         title: "project 1",
         subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
         img: "https://bulma.io/images/placeholders/1280x960.png"
     },
     {
+        id: 2,
         title: "project 2",
         subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
         img: "https://bulma.io/images/placeholders/1280x960.png"
     },
     {
+        id: 3,
         title: "project 3",
         subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
         img: "https://bulma.io/images/placeholders/1280x960.png"
     },
     {
+        id: 4,
         title: "project 4",
         subtitle: "Donec rhoncus aliquet tellus non dignissim. Fusce dictum, nisi ut fringilla consectetur, est velit ultricies nulla, sed lobortis nisi metus eu lorem. Proin malesuada faucibus metus id tristique. Pellentesque in tortor a velit porttitor aliquet.",
         img: "https://bulma.io/images/placeholders/1280x960.png"
@@ -35,7 +39,12 @@ app.engine('hbs', expressHandlebars({
 }))
 
 app.get('/', function(request, response){
-    response.render('home.hbs')
+    const recentProjects = [...projects]
+    recentProjects.length = 3;
+    const model = {
+        recentProjects
+    }
+    response.render('home.hbs', model)
 })
 
 app.get('/projects', function(request, response){
@@ -47,6 +56,15 @@ app.get('/projects', function(request, response){
 
 app.get('/about', function(request, response){
     response.render('about.hbs')
+})
+
+app.get('/project/:id', function(request, response) {
+    const id = request.params.id
+    const project = projects.find((p) => p.id == id)
+    const model = {
+        project
+    }
+    response.render('project.hbs', model)
 })
 
 app.listen(8080)
